79c166daa48ed375e75a5b26ead9e50c56a3616a
xp <xp@Android011>
2016-09-27 19:52:33 +0200

    Para Alex :)

---
+++ src/DOM.java
@@ -0,0 +1,95 @@
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xp
+ */
+public class DOM {
+    Document doc;
+    
+    
+     public int abrir_XML_DOM (File fichero) {
+         doc=null;
+ //doc representa al árbol DOM
+try{
+//Se crea un objeto DocumentBuiderFactory.
+DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+//El modelo DOM no debe contemplar los comentarios que tenga el XML.
+factory.setIgnoringComments(true);
+//Ignora los espacios en blanco que tenga el documento
+factory.setIgnoringElementContentWhitespace(true);
+//Crea un objeto DocumentBuilder para cargar en él la estructura de árbol DOM
+//a partir del XML seleccionado.
+DocumentBuilder builder=factory.newDocumentBuilder();
+//Interpreta (parsear) el documento XML (File) y genera el DOM equivalente.
+doc=(Document) builder.parse(fichero); 
+//Ahora doc apunta al árbol DOM listo para ser recorrido.
+return 0;
+} catch (Exception e){
+ e.printStackTrace();
+ return -1;
+}
+}
+     
+     protected String[] procesarLibro(Node n){
+String datos[]= new String[3];
+Node ntemp=null;
+int contador=1;
+//Obtiene el valor del primer atributo del nodo (uno en este ejemplo)
+datos[0]=n.getAttributes().item(0).getNodeValue();
+//Obtiene los hijos del Libro (titulo y autor)
+NodeList nodos=n.getChildNodes();
+for (int i=0; i<nodos.getLength(); i++) {
+ntemp = nodos.item(i);
+if(ntemp.getNodeType()==Node.ELEMENT_NODE){
+//IMPORTANTE: para obtener el texto con el título y autor se accede al
+// nodo TEXT hijo de ntemp y se saca su valor.
+datos[contador] = ntemp.getChildNodes().item(0).getNodeValue();
+contador++;
+ }
+}
+return datos;
+} 
+     
+     
+     public String recorrerDOMyMostrar(Document doc){
+String datos_nodo[]=null;
+String salida="";
+Node node;
+
+//Obtiene el primero nodo del DOM (primer hijo)
+Node raiz= doc.getFirstChild();
+//Obtiene una lista de nodos con todos los nodos hijo del raíz.
+NodeList nodelist=raiz.getChildNodes();
+//Procesa los nodos hijo
+for (int i=0; i<nodelist.getLength(); i++) {
+node = nodelist.item(i);
+if (node.getNodeType()==Node.ELEMENT_NODE){
+//Es un nodo libro
+datos_nodo = procesarLibro(node);
+salida=salida + "\n " + "Publicado en: " + datos_nodo[0];
+salida=salida + "\n " + "El autor es: " + datos_nodo[2];
+salida=salida + "\n " + "El título es: " + datos_nodo[1];
+salida=salida + "\n -------------------";
+ }
+}
+return salida;
+}
+
+     
+  
+     
+}
